7d93f680f59c4ee7c92bc64557fdfa937fedc93a
xray <creator@darkmattercreations.com>
2019-04-01 15:43:10 -0500

    completed bowling game kata with @christophermanahan and @olgaboiar

--- lib/elixir_bowling_game.ex
+++ lib/elixir_bowling_game.ex
@@ -14,6 +14,30 @@ defmodule ElixirBowlingGame do
   """
 
   def calculate_score(game) do
-    0
+    calculate_score(game, 0)
+  end
+
+  def calculate_score([r_one, r_two | []], acc) do
+    r_one + r_two + acc
+  end
+
+  def calculate_score([r_one, r_two, r_three | []], acc) when r_one == 10 do
+    r_one + r_two + r_three + acc
+  end
+
+  def calculate_score([r_one, r_two, r_three | remaining], acc) when r_one + r_two == 10 do
+    calculate_score([r_three | remaining], r_one + r_two + r_three + acc)
+  end
+
+  def calculate_score([r_one, r_two, r_three | remaining], acc) when r_one == 10 do
+    calculate_score([r_two, r_three | remaining], r_one + r_two + r_three + acc)
+  end
+
+  def calculate_score([r_one, r_two | remaining], acc) do
+    calculate_score(remaining, r_one + r_two + acc)
+  end
+
+  def calculate_score(_game, acc) do
+    acc
   end
 end
--- test/elixir_bowling_game_test.exs
+++ test/elixir_bowling_game_test.exs
@@ -3,7 +3,52 @@ defmodule ElixirBowlingGameTest do
   doctest ElixirBowlingGame
 
   test "score is 0 when given a gutter game" do
-    game = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    game = List.duplicate(0, 20)
     assert ElixirBowlingGame.calculate_score(game) == 0
+  end
+
+  test "it can score a game with a single roll" do
+    game = [8 | List.duplicate(0, 19)]
+    assert ElixirBowlingGame.calculate_score(game) == 8
+  end
+
+  test "it can score a game with two rolls" do
+    game = [7, 2 | List.duplicate(0, 18)]
+    assert ElixirBowlingGame.calculate_score(game) == 9
+  end
+
+  test "it can score a game with one spare" do
+    game = [1, 9, 5 | List.duplicate(0, 17)]
+    assert ElixirBowlingGame.calculate_score(game) == 20
+  end
+
+  test "it can score a game with one spare AND MORE ROLLS" do
+    game = [1, 9, 5, 4 | List.duplicate(0, 16)]
+    assert ElixirBowlingGame.calculate_score(game) == 24
+  end
+
+  test "it can score a game with two spares" do
+    game = [1, 9, 5, 4, 7, 3, 2 | List.duplicate(0, 13)]
+    assert ElixirBowlingGame.calculate_score(game) == 38
+  end
+
+  test "it can score a game with a strike" do
+    game = [10, 5, 1 | List.duplicate(0, 16)]
+    assert ElixirBowlingGame.calculate_score(game) == 22
+  end
+
+  test "it can score spare at the end of the game" do
+    game = List.duplicate(0, 18) ++ [8, 2, 3]
+    assert ElixirBowlingGame.calculate_score(game) == 13
+  end
+
+  test "it can score perfect game" do
+    game = List.duplicate(10, 12)
+    assert ElixirBowlingGame.calculate_score(game) == 300
+  end
+
+  test "it can score a typical game" do
+    game = [6, 3, 6, 4, 6, 2, 4, 6, 10, 10, 3, 2, 5, 5, 4, 3, 3, 1]
+    assert ElixirBowlingGame.calculate_score(game) == 121
   end
 end
